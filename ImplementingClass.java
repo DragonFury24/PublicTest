377ff9d4f4cf95cb81adce50a3700272abc56c96
DragonFury24 <dennisdnguyen19@gmail.com>
2019-03-05 23:48:42 -0500

    Add implementing class

---
+++ ImplementingClass.java
@@ -0,0 +1,14 @@
+public class ImplementingClass extends Interface {
+	public static void staticMethod() {
+		System.out.println("This is a static method");
+	}
+	
+	public void nonStaticMethod() {
+		System.out.println("This is a non static method");
+	}
+
+	public void methodWithArguments(int num1, int num2, String name) {
+		System.out.println("When implementing a method from the interface with arguments
+					you need to have the same arguments");
+	}
+}
